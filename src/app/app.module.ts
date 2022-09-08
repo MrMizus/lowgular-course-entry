2ef92014196db3cc41e3b16a039af220ce4f9bfc
Michał-laptop <jankowski.michal96@gmail.com>
2022-09-08 17:54:05 +0200

    feat: Added form with employee entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,21 @@ import {EmployeesFacesComponentModule} from "./ui/employees-faces/employees-face
 import {EmployeeServiceModule} from "./services/employee.service-module";
 import {ProjectsServiceModule} from "./services/projects.service-module";
 import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {EmployeeFormComponentModule} from "./ui/employee-form/employee-form.component-module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeServiceModule,
-    ProjectsServiceModule,
-    EmployeesFacesComponentModule,
-    ProjectListComponentModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        EmployeeListComponentModule,
+        EmployeeServiceModule,
+        ProjectsServiceModule,
+        EmployeesFacesComponentModule,
+        ProjectListComponentModule,
+        EmployeeFormComponentModule,
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })

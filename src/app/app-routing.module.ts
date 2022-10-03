0f651f4a537e96589ae653d73ee3928e889d71cf
MrMizus <jankowski.michal96@gmail.com>
2022-10-03 19:42:31 +0200

    feat: added routing by id and name in list to details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './ui/home/home.component';
 import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
 import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './ui/employee-details/employee-details.component';
 import { HomeComponentModule } from './ui/home/home.component-module';
 import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 import { EmployeesServiceModule } from './services/employees.service-module';
 import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeDetailsComponentModule } from './ui/employee-details/employee-details.component-module';
 
-const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'create-employee', component: EmployeeFormComponent }, { path: 'employee-list', component: EmployeeListComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'create-employee', component: EmployeeFormComponent }, { path: 'employee-list', component: EmployeeListComponent }, { path: 'employee-details/:id/:name', component: EmployeeDetailsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomeComponentModule, EmployeeFormComponentModule, EmployeesServiceModule, EmployeeListComponentModule],
+  imports: [RouterModule.forRoot(routes), HomeComponentModule, EmployeeFormComponentModule, EmployeesServiceModule, EmployeeListComponentModule, EmployeeDetailsComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
